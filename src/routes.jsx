88ccf91ee6c4fd0775c6f19abd224c9f49972401
Ivonei Junior <ivoneijr@gmail.com>
2018-07-15 18:07:36 -0300

    add some routers

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,13 +3,34 @@ import {
   BrowserRouter as Router,
   hashHistory,
   Route,
+  Link,
 } from 'react-router-dom';
 
 import Chat from './components/Chat/index.jsx';
+import Home from './components/Home/index.jsx';
+import About from './components/About/index.jsx';
 
 const Routes = () => (
   <Router history={hashHistory}>
-    <Route component={Chat} />
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/chat">Chat</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/chat" component={Chat} />
+    </div>
   </Router>
 );
 
